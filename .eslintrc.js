4339bea393cd981628f28cba0c0bb26923bb2c29
fgarcia <gfabian757@gmail.com>
2025-02-20 10:17:18 -0600

    chore: add basic deps and clean init files

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'prettier'],
+  ignorePatterns: ['/dist/*'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
